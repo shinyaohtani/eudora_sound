e271ae01fb0465d28d4260b3146e55798219f390
shinyaohtani <shinya_ohtani@yahoo.co.jp>
2020-05-18 14:38:15 +0900

    v1.0.0

--- lib/eudora_sound/version.rb
+++ lib/eudora_sound/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module EudoraSound
-  VERSION = '0.9.0'
+  VERSION = '1.0.0'
   DESCRIPTION = <<~DESC
     This plays the Eudora mailer's sound. Use this to
     notify the end of processing etc.
